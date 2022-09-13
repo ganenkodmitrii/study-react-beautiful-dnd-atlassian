080b3e1fd69217455bdfde18eff7a6aa3dda15e8
Ganenco Dmitri <ganenko.dmitrii@gmail.com>
2022-09-13 14:55:58 +0300

    Added react-beautiful-dnd

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export const App: React.FC = () => {
-  return <div>test</div>;
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
 };
+
+export default App;
